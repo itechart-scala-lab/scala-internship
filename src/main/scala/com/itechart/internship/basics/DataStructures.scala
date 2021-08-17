da24a63303521055a814d408553b3e33bdc57cc6
Hleb <gleb.streltsov.4by@gmail.com>
2021-08-17 14:46:24 +0300

    Update data structures lection

--- src/main/scala/com/itechart/internship/basics/DataStructures.scala
+++ src/main/scala/com/itechart/internship/basics/DataStructures.scala
@@ -187,39 +187,7 @@ object DataStructures {
   // - takeWhile
   // - zip
 
-  // Exercise 4: Return a set with all subsets of the provided set `set` with `n` elements
-  // For example, `allSubsetsOfSizeN(Set(1, 2, 3), 2) == Set(Set(1, 2), Set(2, 3), Set(1, 3))`.
-  // Hints for implementation:
-  //   - Handle the trivial case where `n == 1`.
-  //   - For other `n`, for each `set` element `elem`, generate all subsets of size `n - 1` from the set
-  //     that don't include `elem`, and add `elem` to them.
-  def allSubsetsOfSizeN[A](set: Set[A], n: Int): Set[Set[A]] = {
-    // replace with correct implementation
-    println(n)
-    Set(set)
-  }
-
-  // Homework
-  //
-  // Implement a special sort which sorts the keys of a map (K) according to their associated
-  // values (V).
-  //
-  // In case of "ties" (equal values) it should group these keys K into Set-s in the results.
-  //
-  // The input is a map where keys (K) are the values to be sorted and values are their associated numeric
-  // values.
-  //
-  // The output is a list (in ascending order according to the associated `Int` values) of tuples of `Set`-s
-  // with values from K, and the associated value V for these values in the `Set`.
-  //
-  // For example:
-  //
-  // Input `Map("a" -> 1, "b" -> 2, "c" -> 4, "d" -> 1, "e" -> 0, "f" -> 2, "g" -> 2)` should result in
-  // output `List(Set("e") -> 0, Set("a", "d") -> 1, Set("b", "f", "g") -> 2, Set("c") -> 4)`.
-  def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = ???
-
-  /** Exploring Functional Sets
-    */
+  /** Exploring Functional Sets */
 
   val aSet = Set(1, 2, 3, 4, 5)
 
@@ -358,4 +326,37 @@ object DataStructures {
 
   def main(args: Array[String]): Unit = {}
 
+  // Homework
+
+  // Task 1
+
+  // For example, `allSubsetsOfSizeN(Set(1, 2, 3), 2) == Set(Set(1, 2), Set(2, 3), Set(1, 3))`.
+  // Hints for implementation:
+  //   - Handle the trivial case where `n == 1`.
+  //   - For other `n`, for each `set` element `elem`, generate all subsets of size `n - 1` from the set
+  //     that don't include `elem`, and add `elem` to them.
+  def allSubsetsOfSizeN[A](set: Set[A], n: Int): Set[Set[A]] = {
+    // replace with correct implementation
+    println(n)
+    Set(set)
+  }
+
+  // Task 2
+
+  // Implement a special sort which sorts the keys of a map (K) according to their associated
+  // values (V).
+  //
+  // In case of "ties" (equal values) it should group these keys K into Set-s in the results.
+  //
+  // The input is a map where keys (K) are the values to be sorted and values are their associated numeric
+  // values.
+  //
+  // The output is a list (in ascending order according to the associated `Int` values) of tuples of `Set`-s
+  // with values from K, and the associated value V for these values in the `Set`.
+  //
+  // For example:
+  //
+  // Input `Map("a" -> 1, "b" -> 2, "c" -> 4, "d" -> 1, "e" -> 0, "f" -> 2, "g" -> 2)` should result in
+  // output `List(Set("e") -> 0, Set("a", "d") -> 1, Set("b", "f", "g") -> 2, Set("c") -> 4)`.
+  def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = ???
 }
