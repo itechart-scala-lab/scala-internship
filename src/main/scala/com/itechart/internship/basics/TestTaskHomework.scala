191a5aa72b918c82ebbc13205f9b8c23dd012ca0
Hleb <gleb.streltsov.4by@gmail.com>
2021-08-17 11:29:40 +0300

    Add possible interface for homework #2

--- src/main/scala/com/itechart/internship/basics/TestTaskHomework.scala
+++ src/main/scala/com/itechart/internship/basics/TestTaskHomework.scala
@@ -1,6 +1,6 @@
 package com.itechart.internship.basics
 
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 object TestTaskHomework {
 
@@ -25,15 +25,72 @@ object TestTaskHomework {
 
   def parseInt(x: String): Try[Int] = Try(x.toInt)
 
-  def main(args: Array[String]): Unit = {
-    parseInt("10") match {
-      case Success(value) => println(value)
-      case Failure(error) => println(error)
-    }
+//  def main(args: Array[String]): Unit = {
+//    parseInt("10") match {
+//      case Success(value) => println(value)
+//      case Failure(error) => println(error)
+//    }
+//
+//    parseInt("asdf") match {
+//      case Success(value) => println(value)
+//      case Failure(error) => println(error)
+//    }
+//  }
 
-    parseInt("asdf") match {
-      case Success(value) => println(value)
-      case Failure(error) => println(error)
-    }
+  // possible interface for your solution below (keep in mind that
+  // it is not mandatory interface, you can change it if you think
+  // it will make your code better and more comfortable to use):
+
+  type ErrorMessage = String
+  final case class RawSpreadsheet(/* add here properties */ )
+  final case class ProcessedSpreadsheet(/* add here properties */ )
+
+  def validateReadPath(readPath:  String): Either[ErrorMessage, String] = ???
+  def validateWritePath(readPath: String): Either[ErrorMessage, String] = ???
+
+  trait SpreadsheetParser {
+    def parse(): Either[ErrorMessage, RawSpreadsheet]
+  }
+
+  class LocalSpreadsheetParser(path: String) extends SpreadsheetParser {
+    override def parse(): Either[ErrorMessage, RawSpreadsheet] = ???
+  }
+
+  trait SpreadsheetProcessor {
+    def process(spreadsheet: RawSpreadsheet): ProcessedSpreadsheet
+  }
+
+  class SimpleSpreadsheetProcessor extends SpreadsheetProcessor {
+    override def process(spreadsheet: RawSpreadsheet): ProcessedSpreadsheet = ???
+  }
+
+  trait SpreadsheetWriter {
+    def write(writePath: String, processedSpreadsheet: ProcessedSpreadsheet): Either[ErrorMessage, RawSpreadsheet]
+  }
+
+  class LocalSpreadsheetWriter() extends SpreadsheetWriter {
+    override def write(
+      writePath:            String,
+      processedSpreadsheet: ProcessedSpreadsheet
+    ): Either[ErrorMessage, RawSpreadsheet] = ???
+  }
+
+  def main(args: Array[String]): Unit = {
+    val readPath  = "get from args - command line args"
+    val writePath = "get from args - command line args"
+
+    for {
+      validReadPath  <- validateReadPath(readPath)
+      validWritePath <- validateWritePath(writePath)
+
+      spreadsheetParser = new LocalSpreadsheetParser(validReadPath)
+      parsedMatrix     <- spreadsheetParser.parse()
+
+      spreadsheetProcessor = new SimpleSpreadsheetProcessor
+      processedSpreadsheet = spreadsheetProcessor.process(parsedMatrix)
+
+      spreadsheetWriter = new LocalSpreadsheetWriter
+      _                <- spreadsheetWriter.write(validWritePath, processedSpreadsheet)
+    } yield ()
   }
 }
