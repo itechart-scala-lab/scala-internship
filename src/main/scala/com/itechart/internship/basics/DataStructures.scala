a986107cd0cca835b75f89534a24fc414a874a5f
Hleb <gleb.streltsov.4by@gmail.com>
2021-08-17 12:28:06 +0300

    Update DataStructures lection

--- src/main/scala/com/itechart/internship/basics/DataStructures.scala
+++ src/main/scala/com/itechart/internship/basics/DataStructures.scala
@@ -15,7 +15,7 @@ object DataStructures {
   val mutableList = scala.collection.mutable.ListBuffer(1, 2, 3)
   mutableList.update(1, -1)
 
-  val immutableList1 = scala.collection.immutable.List(1, 2, 3)
+  val immutableList1       = scala.collection.immutable.List(1, 2, 3)
   val updatedImmutableList = immutableList1.updated(1, -1)
 
   val doTheyHaveEqualContents1 = mutableList == updatedImmutableList // true
@@ -42,13 +42,13 @@ object DataStructures {
   val emptyList2 = List()
   val emptyList3 = List.empty
 
-  val prepend4 = 4 :: immutableList2 // 4 :: 1 :: 2 :: 3 :: Nil
-  val prepend42 = 5 :: immutableList2 // 5 :: 1 :: 2 :: 3 :: Nil
+  val prepend4   = 4 :: immutableList2 // 4 :: 1 :: 2 :: 3 :: Nil
+  val prepend42  = 5 :: immutableList2 // 5 :: 1 :: 2 :: 3 :: Nil
   val tailOfList = immutableList2.tail // 2 :: 3 :: Nil
 
   val joinLists = immutableList2 ::: List(8, 9) // 1 :: 2 :: 3 :: 8 :: 9 :: Nil
 
-  val headOfList1 = Try(emptyList1.head)// what will happen here?!
+  val headOfList1 = Try(emptyList1.head) // what will happen here?!
   val headOfList2 = emptyList1.headOption // None
   val headOfList3 = immutableList2.headOption // Some(1)
 
@@ -64,6 +64,9 @@ object DataStructures {
   val vector3 = vector1 :+ 6 // append
   val vector4 = vector2 ++ vector3 // concatenate
 
+  val vectorValue1 = vector4(0)
+  val vectorValue2 = vector4(3)
+
   // Sets
   //
   // Sets are data structures that don't contain any duplicate elements.
@@ -77,7 +80,7 @@ object DataStructures {
   val moreVegetables = vegetables + "avocado"
   val lessVegetables = moreVegetables - "peppers"
 
-  // Exercise. Write a function that checks if all values in a `List` are equal.
+  // Exercise 1. Write a function that checks if all values in a `List` are equal.
   // Think about what you think your function should return if `list` is empty, and why.
   def allEqual[T](list: List[T]): Boolean = {
     false // TODO: implement
@@ -96,11 +99,11 @@ object DataStructures {
 
   val vegetablePrices = Map(
     "tomatoes" -> 4,
-    "peppers" -> 5,
-    "olives" -> 17,
+    "peppers"  -> 5,
+    "olives"   -> 17,
   )
 
-  val moreVegetablePrices = vegetablePrices + ("pumpkins" -> 3)
+  val moreVegetablePrices  = vegetablePrices + ("pumpkins" -> 3)
   val lessVegetableWeights = vegetableWeights - "pumpkins"
 
   val questionableMap = vegetableWeights ++ vegetablePrices
@@ -108,24 +111,24 @@ object DataStructures {
   // Question. Why should `questionableMap` be considered questionable?
 
   val vegetableAmounts = Map(
-    "tomatoes" -> 17,
-    "peppers" -> 234,
-    "olives" -> 32,
+    "tomatoes"  -> 17,
+    "peppers"   -> 234,
+    "olives"    -> 32,
     "cucumbers" -> 323,
   )
 
-  val tomatoAmount: Int = vegetableAmounts("tomatoes")
-  val tomatoAmountOpt: Option[Int] = vegetableAmounts.get("tomatoes")
-  val carrotAmountWithDefault: Int = vegetableAmounts.getOrElse("carrots", 0)
+  val tomatoAmount:            Int         = vegetableAmounts("tomatoes")
+  val tomatoAmountOpt:         Option[Int] = vegetableAmounts.get("tomatoes")
+  val carrotAmountWithDefault: Int         = vegetableAmounts.getOrElse("carrots", 0)
 
-  // Exercise. Calculate the total cost of all vegetables, taking vegetable amounts (in units) from
+  // Exercise 2. Calculate the total cost of all vegetables, taking vegetable amounts (in units) from
   // `vegetableAmounts` and prices per unit from `vegetablePrices`. Assume the price is 10 if not available
   // in `vegetablePrices`.
   val totalVegetableCost: Int = {
     17 // implement here
   }
 
-  // Exercise. Given the vegetable weights (per 1 unit of vegetable) in `vegetableWeights` and vegetable
+  // Exercise 3. Given the vegetable weights (per 1 unit of vegetable) in `vegetableWeights` and vegetable
   // amounts (in units) in `vegetableAmounts`, calculate the total weight per type of vegetable, if known.
   //
   // For example, the total weight of "olives" is 2 * 32 == 64.
@@ -134,9 +137,9 @@ object DataStructures {
   }
 
   // Ranges and Sequences
-  val inclusiveRange: Seq[Int] = 2 to 4    // 2, 3, 4, or <=
+  val inclusiveRange: Seq[Int] = 2 to 4 // 2, 3, 4, or <=
   val exclusiveRange: Seq[Int] = 2 until 4 // 2, 3, or <
-  val withStep: Seq[Int] = 2 to 40 by 7    // 2, 9, 16, 23, 30, 37
+  val withStep:       Seq[Int] = 2 to 40 by 7 // 2, 9, 16, 23, 30, 37
 
   // Seq, IndexedSeq and LinearSeq traits are implemented by many collections and contain various useful
   // methods. See https://docs.scala-lang.org/overviews/collections/seqs.html in case you are interested
@@ -184,7 +187,7 @@ object DataStructures {
   // - takeWhile
   // - zip
 
-  // Exercise: Return a set with all subsets of the provided set `set` with `n` elements
+  // Exercise 4: Return a set with all subsets of the provided set `set` with `n` elements
   // For example, `allSubsetsOfSizeN(Set(1, 2, 3), 2) == Set(Set(1, 2), Set(2, 3), Set(1, 3))`.
   // Hints for implementation:
   //   - Handle the trivial case where `n == 1`.
@@ -215,27 +218,25 @@ object DataStructures {
   // output `List(Set("e") -> 0, Set("a", "d") -> 1, Set("b", "f", "g") -> 2, Set("c") -> 4)`.
   def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = ???
 
+  /** Exploring Functional Sets
+    */
 
-  /**
-    * Exploring Functional Sets
-     */
+  val aSet = Set(1, 2, 3, 4, 5)
 
-  val aSet = Set(1,2,3,4,5)
-
-/*
+  /*
   The critical API of a set consists of
 
-  * the ability to tell whether an item is in the set or not
-  * the ability to add an element to a set (and if it exists, don’t add it again)
-  * the ability to remove an element from the set (and if it doesn’t exist, don’t remove it again, of course)
-*/
+   * the ability to tell whether an item is in the set or not
+   * the ability to add an element to a set (and if it exists, don’t add it again)
+   * the ability to remove an element from the set (and if it doesn’t exist, don’t remove it again, of course)
+   */
 
 //  Let’s concentrate on the first capability at the moment. The way we tell whether an element is in the set is by calling contains, or apply:
 
   aSet.contains(2) // true
   aSet(2) // also true
 
-/*
+  /*
   Notice that the apply method makes the set “callable” like a function.
   At the same time, that invocation always returns a value (true or false), for any argument you pass to it.
 
@@ -267,41 +268,40 @@ object DataStructures {
     ...
   }*/
 
-  /**
-    * Writing a Small Functional Set
+  /** Writing a Small Functional Set
     */
 
 //  Here’s a small experiment — let’s write a small implementation of a set that implements the functional interface A => Boolean:
 
   trait RSet[A] extends (A => Boolean) {
-    def apply(x: A): Boolean = contains(x)
+    def apply(x:    A): Boolean = contains(x)
     def contains(x: A): Boolean
-    def +(x: A): RSet[A]
-    def -(x: A): RSet[A]
+    def +(x:        A): RSet[A]
+    def -(x:        A): RSet[A]
   }
 
-/*
+  /*
   The main trait implements the crucial Set API:
 
-  * testing if an element is in the set
-  * adding an element
-  * removing an element
+   * testing if an element is in the set
+   * adding an element
+   * removing an element
 
   Let’s then continue with an implementation of an empty set, correctly typed.
   The standard library uses an object typed with Set[Any] and then type-checked via casting, but let’s use a small case class for our experiment:
-*/
+   */
 
   case class REmpty[A]() extends RSet[A] {
     override def contains(x: A) = false
-    def +(x: A): RSet[A] = ???
-    def -(x: A): RSet[A] = this
+    def +(x:                 A): RSet[A] = ???
+    def -(x:                 A): RSet[A] = this
   }
 
   /*
   The implementation of 2 out of 3 methods is easy:
 
-  * the set doesn’t contain anything, so contains(x) == false for all x in A
-  * the set can’t remove anything, so return the same set
+   * the set doesn’t contain anything, so contains(x) == false for all x in A
+   * the set can’t remove anything, so return the same set
   We’ll come back to the third method shortly.
 
   Let’s now consider a set given by a property, i.e. similarly to how we were taught in math classes.
@@ -310,20 +310,20 @@ object DataStructures {
   Some sets may be finite, or infinite, some may be countable (or not).
 
   We’re not going to dive super-deep into the rabbit hole — that’s a job for my students in the advanced Scala course — but let’s try declaring a small set in terms of the property of their elements (a property-based set):
-*/
+   */
 
   case class PBSet[A](property: A => Boolean) extends RSet[A] {
     def contains(x: A): Boolean = property(x)
-    def +(x: A): RSet[A] = new PBSet[A](e => property(e) || e == x)
-    def -(x: A): RSet[A] = if (contains(x)) new PBSet[A](e => property(e) && e != x) else this
+    def +(x:        A): RSet[A] = new PBSet[A](e => property(e) || e == x)
+    def -(x:        A): RSet[A] = if (contains(x)) new PBSet[A](e => property(e) && e != x) else this
   }
 
-/*
+  /*
   Let’s look at the main API methods:
 
-  * this set is all about the property of the elements, so contains returns true only if that property is satisfied
-  * adding an element means adjusting the property so that it also holds true for the element we want to add
-  * removing an element means adjusting the property so that it definitely returns false for the element we’re removing
+   * this set is all about the property of the elements, so contains returns true only if that property is satisfied
+   * adding an element means adjusting the property so that it also holds true for the element we want to add
+   * removing an element means adjusting the property so that it definitely returns false for the element we’re removing
   And that’s it! The set will not contain duplicates nor change if we try removing a non-existent element, because neither makes any sense now.
 
   Coming back to REmpty’s add method, it will look like this:
@@ -332,12 +332,12 @@ object DataStructures {
   A single-element set is a property-based set, where the property only returns true for that particular element.
 
   Of course, we can test this out. Let’s make a small helper method that will allow us to build sets more easily:
-*/
+   */
 
   object RSet {
     def apply[A](values: A*) = values.foldLeft[RSet[A]](new REmpty())(_ + _)
   }
-/*
+  /*
   Then in main, we can run some tests:
 
   val first5Elements: RSet[Int] = REmpty[Int]() + 1 + 2 + 3 + 4 + 5
@@ -354,7 +354,8 @@ object DataStructures {
   For example, the set of even natural numbers is now trivial:
 
   val allEvens = PBSet[Int](_ % 2 == 0)
-*/
+   */
 
+  def main(args: Array[String]): Unit = {}
 
 }
