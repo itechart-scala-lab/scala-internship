05ba6c55cec85a98a5ec233047da3bd0d537389c
Hleb <gleb.streltsov.4by@gmail.com>
2021-08-24 13:58:18 +0300

    Fix implicits import issue for Refined

--- src/main/scala/com/itechart/internship/error_handling/RefinedBasics.scala
+++ src/main/scala/com/itechart/internship/error_handling/RefinedBasics.scala
@@ -1,9 +1,10 @@
 package com.itechart.internship.error_handling
 
+import eu.timepit.refined._
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.numeric.{Even, Negative, NonNegative, Odd, Positive}
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric._
 import eu.timepit.refined.string.MatchesRegex
-import eu.timepit.refined.{refineV, W}
 
 object RefinedBasics {
 
